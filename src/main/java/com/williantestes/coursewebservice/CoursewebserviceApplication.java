da4309fa741702a756946bea255dc26e8861a533
Willian <willian.neibertz@gmail.com>
2023-09-29 17:28:22 -0300

    project created

---
+++ src/main/java/com/williantestes/coursewebservice/CoursewebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.williantestes.coursewebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursewebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursewebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/williantestes/coursewebservice/CoursewebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.williantestes.coursewebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursewebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
