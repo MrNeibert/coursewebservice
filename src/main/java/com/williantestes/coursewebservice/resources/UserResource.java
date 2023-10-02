0c88c8a166a57d042f4e5c3a8822572f674779a0
Willian <willian.neibertz@gmail.com>
2023-10-02 11:16:49 -0300

    User entity and resource

---
+++ src/main/java/com/williantestes/coursewebservice/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.williantestes.coursewebservice.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.williantestes.coursewebservice.entities.User;  
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+      User u = new User(1L, "Maria", "maria@gmail.com", "99981267-123", "qwerty");
+      return ResponseEntity.ok().body(u);
+    }
+}
