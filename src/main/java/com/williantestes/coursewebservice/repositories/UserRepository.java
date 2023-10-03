766f34c13cab2a0cd811d8000ffb3715f94991dc
Willian <willian.neibertz@gmail.com>
2023-10-03 11:10:48 -0300

    Service layer, component registration

--- src/main/java/com/williantestes/coursewebservice/repositories/UserRepository.java
+++ src/main/java/com/williantestes/coursewebservice/repositories/UserRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.williantestes.coursewebservice.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User,Long>{
   
 }
