afbc7a2775af550172e16fe896b5d6222de5548a
Willian <willian.neibertz@gmail.com>
2023-10-23 10:13:11 -0300

    Exception handling - delete

--- src/main/java/com/williantestes/coursewebservice/services/UserService.java
+++ src/main/java/com/williantestes/coursewebservice/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.williantestes.coursewebservice.entities.User;
 import com.williantestes.coursewebservice.repositories.UserRepository;
+import com.williantestes.coursewebservice.services.exceptions.DatabaseException;
 import com.williantestes.coursewebservice.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -29,8 +32,15 @@ public class UserService {
     return repository.save(obj);
   }
 
-  public void delete(Long Id) {
-    repository.deleteById(Id);
+  public void delete(Long id) {
+    try {
+      repository.deleteById(id);
+    } catch (EmptyResultDataAccessException e) {
+      throw new ResourceNotFoundException(id);
+      
+    } catch(DataIntegrityViolationException e) {
+      throw new DatabaseException(e.getMessage());
+    }
   }
 
   public User update(Long id, User obj) {
